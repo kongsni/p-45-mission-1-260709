85d8b8cb90c5d4f2ef0467044624fde1e6247f62
kongsni <binybinybiny03@gmail.com>
2026-07-09 14:20:18 +0900

    [BE14]송영빈

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요 저는 송영빈이고 24세입니다 잘 부탁드립니다");
+        System.out.println("안녕하세요 저는 송영빈이고 24세입니다 잘 부탁드립니다!");
     }
 }
